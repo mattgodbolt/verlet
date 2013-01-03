68f893fcf09ad52105243ddc88e50b90d9c1a8b4
Matt Godbolt <mgodbolt@drwholdings.com>
2013-01-03 17:19:54 -0600

    Fixed up refactored code

--- verlet.js
+++ verlet.js
@@ -1,111 +1,106 @@
 var canvas = null;
 var ctx = null;
 
-var Height = 480;
-var Width = 640;
-var Gravity = 0.18;
-var Friction = 0.9995;
-var Elastic = 0.997;
-
 function Point(x, y) {
     this.x = x;
     this.y = y;
-    this.add = function(p) {
-        this.x += p.x;
-        this.y += p.y;
-        return this;
-    }
-    this.sub = function(p) {
-        this.x -= p.x;
-        this.y -= p.y;
-    }
-    this.mul = function(scale) {
-        this.x *= scale;
-        this.y *= scale;
+    this.plus = function(p) { return new Point(this.x + p.x, this.y + p.y); };
+    this.minus = function(p) { return new Point(this.x - p.x, this.y - p.y); };
+    this.scale = function(scale) { return new Point(this.x * scale, this.y * scale); };
+    this.assign = function(p) { this.x = p.x; this.y = p.y; };
+    this.distanceTo = function(p) {
+        var xDist = this.x - p.x;
+        var yDist = this.y - p.y;
+        return Math.sqrt(xDist * xDist + yDist * yDist);
     }
 };
 
+Point.midpoint = function(p1, p2) {
+    return p1.plus(p2).scale(0.5);
+}
+
+Point.constrainDistance = function(p1, p2, distFunc) {
+    var actualDist = p1.distanceTo(p2);
+    var constrainDist = distFunc(actualDist);
+    if (constrainDist === actualDist) return;
+    var scale = constrainDist / actualDist;
+    var halfVector = p1.minus(p2).scale(0.5 * scale);
+    var midPoint = Point.midpoint(p1, p2);
+    p1.assign(midPoint.plus(halfVector));
+    p2.assign(midPoint.minus(halfVector));
+}
+
+var Height = 480;
+var Width = 640;
+var Gravity = new Point(0, 0.18);
+var Friction = 0.9995;
+var Elastic = 0.994;
+
 function Static(xPos, yPos) {
-    this.x = xPos;
-    this.y = yPos;
+    this.pos = new Point(xPos, yPos);
 
     this.draw = function() {};
     this.move = function() {
-        this.x = xPos;
-        this.y = yPos;
+        this.pos.x = xPos;
+        this.pos.y = yPos;
     };
     this.constrain = function() {};
+    this.collide = function() {};
 };
 
 function Mouse() {
-    this.x = 0;
-    this.y = 0;
+    this.pos = new Point(0, 0);
 
-    var mouseX = 0;
-    var mouseY = 0;
+    var mousePos = new Point(0,0);
     canvas.addEventListener('mousemove', function(evt) {
         var rect = canvas.getBoundingClientRect();
-        mouseX = evt.clientX - rect.left;
-        mouseY = evt.clientY - rect.top;
+        mousePos.x = evt.clientX - rect.left;
+        mousePos.y = evt.clientY - rect.top;
     });
 
     this.draw = function() {};
     this.move = function() {
-        this.x = mouseX;
-        this.y = mouseY;
+        this.pos.assign(mousePos);
     };
     this.constrain = function() {};
     this.collide = function() {};
 };
 
 function Circle(xPos, yPos) {
-    this.x = xPos;
-    this.y = yPos;
+    this.pos = new Point(xPos, yPos);
     this.radius = 20;
 
-    this.oldX = this.x;
-    this.oldY = this.y;
+    this.oldPos = new Point(xPos, yPos);
 
     this.draw = function() {
         ctx.fillStyle = "#a22";
         ctx.strokeStyle = "#000";
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fill();
         ctx.stroke();
     };
 
     this.move = function() {
-        var velX = this.x - this.oldX;
-        var velY = this.y - this.oldY + Gravity;
-        this.oldX = this.x;
-        this.oldY = this.y;
-        this.x += velX * Friction;
-        this.y += velY * Friction;
+        var velocity = this.pos.minus(this.oldPos).plus(Gravity).scale(Friction);
+        this.oldPos.assign(this.pos);
+        this.pos = this.pos.plus(velocity);
     };
 
     this.constrain = function() {
-        if (this.x < this.radius) this.x = this.radius;
-        if (this.x > Width - this.radius) this.x = Width - this.radius;
-        if (this.y < this.radius) this.y = this.radius;
-        if (this.y > Height - this.radius) this.y = Height - this.radius;
+        if (this.pos.x < this.radius) this.pos.x = this.radius;
+        if (this.pos.x > Width - this.radius) this.pos.x = Width - this.radius;
+        if (this.pos.y < this.radius) this.y = this.pos.radius;
+        if (this.pos.y > Height - this.radius) this.pos.y = Height - this.radius;
     };
+
     this.collide = function(other) {
         if (other.radius !== undefined) {
-            var xDist = this.x - other.x;
-            var yDist = this.y - other.y;
-            var distSqr = xDist * xDist + yDist * yDist;
-            var dist = Math.sqrt(distSqr);
             var minDist = this.radius + other.radius;
-            if (dist >= minDist) return;
-            var ratio = (minDist / dist);
-            var midX = (this.x + other.x) / 2.0;
-            var midY = (this.y + other.y) / 2.0;
-            this.x = midX + (xDist / 2) * ratio;
-            this.y = midY + (yDist / 2) * ratio;
-            other.x = midX - (xDist / 2) * ratio;
-            other.y = midY - (yDist / 2) * ratio;
+            Point.constrainDistance(this.pos, other.pos, function(dist) { 
+                return Math.max(dist, minDist); 
+            });
         }
     };
 }
@@ -116,8 +111,8 @@ function Rope(from, to, maxDist) {
 
     this.draw = function() {
         ctx.beginPath();
-        ctx.moveTo(this.from.x, this.from.y);
-        ctx.lineTo(this.to.x, this.to.y);
+        ctx.moveTo(this.from.pos.x, this.from.pos.y);
+        ctx.lineTo(this.to.pos.x, this.to.pos.y);
         ctx.closePath();
         ctx.stroke();
     }
@@ -125,18 +120,10 @@ function Rope(from, to, maxDist) {
     this.move = function() {};
 
     this.constrain = function() {
-        var xDist = this.from.x - this.to.x;
-        var yDist = this.from.y - this.to.y;
-        var distSqr = xDist * xDist + yDist * yDist;
-        var dist = Math.sqrt(distSqr);
-        if (dist < maxDist) return;
-        var ratio = (maxDist / dist) * Elastic;
-        var midX = (this.from.x + this.to.x) / 2.0;
-        var midY = (this.from.y + this.to.y) / 2.0;
-        this.from.x = midX + (xDist / 2) * ratio;
-        this.from.y = midY + (yDist / 2) * ratio;
-        this.to.x = midX - (xDist / 2) * ratio;
-        this.to.y = midY - (yDist / 2) * ratio;
+        Point.constrainDistance(this.from.pos, this.to.pos, function(dist) { 
+            if (dist < maxDist) return dist;
+            return dist + (maxDist - dist) * Elastic;
+        });
     };
     this.collide = function() {};
 };
@@ -146,7 +133,7 @@ function init() {
     //scene = [new Static(80, 20)];
     scene = [new Mouse()];
 
-    for (var i = 0; i < 9; ++i) {
+    for (var i = 0; i < 10; ++i) {
         scene.push(new Circle(80 + 25 * i, 20 + 45 * i));
     }
     var numRopes = scene.length;
